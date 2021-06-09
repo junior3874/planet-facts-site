059783e5ca522f0b54a3b158fd3d1afa2b86e5cd
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-06-09 20:31:54 -0300

    finalized menu mobile adaptation

--- src/templates/menuMobile.js
+++ src/templates/menuMobile.js
@@ -2,17 +2,12 @@ import getPlanetOptionMobileTemplate from "./planetOptionMobile.js";
 
 const menuMobileIcon = "./src/assets/icon-hamburger.svg";
 export default function getMenuMobileTemplate(planetNames) {
-  const planets = planetNames.map((planet) =>
-    getPlanetOptionMobileTemplate(planet)
-  );
-  planets.join(" ");
-
   return `
     <a id="menu_select_planet">
         <img class="icon-hamburger"src="${menuMobileIcon}" alt="">
     </a>
     <ul id="list_planets">
-      ${planets}
+      ${getPlanetOptionMobileTemplate(planetNames)}
     </ul>
   `;
 }
