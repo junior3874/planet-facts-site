50e56617ef7f909de7dbbe03e51bf7c0feb1b038
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-06-09 20:32:12 -0300

    finalized template

--- src/templates/planetOptionMobile.js
+++ src/templates/planetOptionMobile.js
@@ -1,9 +1,15 @@
-export default function getPlanetOptionMobileTemplate(name) {
-  return `
-    <li class="planet_option_mobile">
-        <div class="planet_option_icon" id="${name}"></div>
+import { constants } from "../_shared/constants.js";
+
+export default function getPlanetOptionMobileTemplate(planets) {
+  const planetsHtmlList = planets.map(
+    (name) =>
+      `
+        <li class="planet_option_mobile">
+        <div id="${name}" class="planet_option_icon" style="background-color:${constants.planetColorsReferences[name]};"></div>
         <span>${name}</span>
         <img src="./src/assets/icon-chevron.svg" alt="go to">
     </li>
-`;
+    `
+  );
+  return planetsHtmlList.join(" ");
 }
