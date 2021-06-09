618c069c0bbacfcc341761b20f2b8b3339118a75
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-06-09 18:57:46 -0300

    added global files from aplication

---
+++ src/_shared/constants.js
@@ -0,0 +1,12 @@
+export default constants = [
+  (planetColorsReferences = {
+    ["Mercury"]: "#419ebb",
+    ["Venus"]: "#eda249",
+    ["Earth"]: "#6d2ed5",
+    ["Mars"]: "#d14c32",
+    ["Jupiter"]: "#d83a34",
+    ["Saturn"]: "#cd5120",
+    ["Uranus"]: "#1ec1a2",
+    ["Neptune"]: "#2d68f0",
+  }),
+];
