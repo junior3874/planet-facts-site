cc69a075b5f98acd8cdf471b995d97a157903e29
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-06-09 18:45:29 -0300

    service now it is fornecing informations from aplication

--- src/controller.js
+++ src/controller.js
@@ -3,11 +3,22 @@ export default class Controller {
     this.service = service;
     this.view = view;
   }
+  planets = [];
 
   static initialize(deps) {
     return new Controller(deps)._initialize();
   }
   _initialize() {
+    this._setupViewEvents();
+  }
+
+  async _setupViewEvents() {
+    const planets = await this.onGetPlanetsFromService();
+    await this.view.loadingPlanets(planets);
     this.view.initializeMobileViewPort();
+  }
+
+  onGetPlanetsFromService() {
+    return this.service.getPlanets();
   }
 }
