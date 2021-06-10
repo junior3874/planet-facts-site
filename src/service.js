3ef9fa9d8962b616631e5317aee2c1b0b8f75042
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-06-09 22:20:09 -0300

    added method for get planet with name

--- src/service.js
+++ src/service.js
@@ -1,3 +1,5 @@
+import Planet from "./entities/planet.js";
+
 export default class Service {
   constructor({ url }) {
     this.url = url;
@@ -7,5 +9,12 @@ export default class Service {
     return fetch(this.url)
       .then((response) => response.json())
       .then((result) => result);
+  }
+  getPlanet(planetName) {
+    return fetch(this.url)
+      .then((response) => response.json())
+      .then((result) =>
+        result.filter((planet = new Planet()) => planet.name == planetName)
+      );
   }
 }
