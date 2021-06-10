7915acd12a6e4a62de44d5812f593fe0e064b9fc
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-06-09 22:17:27 -0300

    changes from props

---
+++ src/entities/planet.js
@@ -0,0 +1,22 @@
+export default class Planet {
+  constructor({
+    image,
+    name,
+    sinopse,
+    wikipedia_link,
+    rotation,
+    revolution,
+    radius,
+    temperature,
+  }) {
+    this.image = image;
+    this.name = name;
+    this.sinopse = sinopse;
+    this.wikipedia_link = wikipedia_link;
+    this.rotation = rotation;
+    this.revolution_time = revolution;
+    this.radius = radius;
+    this.temperature = temperature;
+    debugger;
+  }
+}
