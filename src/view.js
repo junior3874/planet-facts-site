793ffe72b344265c580e1bbe3578e783b9716d92
junior3874 <wellingtonjr53@outlook.com.br>
2021-06-10 16:07:13 -0300

    removed scroll from body becausa modal is open

--- src/view.js
+++ src/view.js
@@ -5,76 +5,80 @@ const mobileViewPort = window.matchMedia("(max-width: 786px)");
 const body = document.querySelector("body");
 
 export default class View {
-  static loadingPlanets(planets) {
-    this.planetNames = planets.map(({ name }) => name);
-    this.planets = planets;
-  }
-  static initializeMobileViewPort() {
-    View._monitoringMobileViewPort();
-    mobileViewPort.addListener(View._monitoringMobileViewPort);
-  }
+	static loadingPlanets(planets) {
+		this.planetNames = planets.map(({ name }) => name);
+		this.planets = planets;
+	}
+	static initializeMobileViewPort() {
+		View._monitoringMobileViewPort();
+		mobileViewPort.addListener(View._monitoringMobileViewPort);
+	}
 
-  static _monitoringMobileViewPort() {
-    View._removeOldContentHeader();
-    mobileViewPort.matches ? View._setMenuMobile() : View._setMenuDesktop();
-  }
+	static _monitoringMobileViewPort() {
+		View._removeOldContentHeader();
+		mobileViewPort.matches ? View._setMenuMobile() : View._setMenuDesktop();
+	}
 
-  static _removeOldContentHeader() {
-    navContent.innerHTML = "";
-  }
+	static _removeOldContentHeader() {
+		navContent.innerHTML = "";
+	}
 
-  static _getElementFromId({ id, baseElement = document }) {
-    const existingItem = baseElement.querySelector(`[id="${id}"]`);
-    return existingItem;
-  }
+	static _getElementFromId({ id, baseElement = document }) {
+		const existingItem = baseElement.querySelector(`[id="${id}"]`);
+		return existingItem;
+	}
 
-  static _getElementsFromClass({ className, baseElement = document }) {
-    const existingItem = baseElement.querySelectorAll(`[class=${className}]`);
-    return existingItem;
-  }
+	static _getElementsFromClass({ className, baseElement = document }) {
+		const existingItem = baseElement.querySelectorAll(`[class=${className}]`);
+		return existingItem;
+	}
 
-  // Menu mobile methods
-  static async _setMenuMobile() {
-    const htmlTemplateMenuDesktop = getMenuMobileTemplate(this.planetNames);
-    navContent.innerHTML = `${htmlTemplateMenuDesktop}`;
+	// Menu mobile methods
+	static async _setMenuMobile() {
+		const htmlTemplateMenuDesktop = getMenuMobileTemplate(this.planetNames);
+		navContent.innerHTML = `${htmlTemplateMenuDesktop}`;
 
-    this.addEventFromBtnListPlanets();
-  }
-  static addEventFromBtnListPlanets() {
-    const btnListPlanets = View._getElementFromId({ id: "menu_select_planet" });
-    btnListPlanets.addEventListener("click", View._eventFromBtnListPlanets);
-  }
+		this.addEventFromBtnOpenModalListPlanets();
+	}
+	static addEventFromBtnOpenModalListPlanets() {
+		const btnListPlanets = View._getElementFromId({ id: "menu_select_planet" });
+		btnListPlanets.addEventListener(
+			"click",
+			View._eventFromBtnOpenModalListPlanets
+		);
+	}
 
-  static _removeScrollFromBody() {
-    body.style.overflow = "hidden";
-  }
-  static _eventFromBtnListPlanets() {
-    const modalListPlanets = View._getElementFromId({ id: "list_planets" });
+	static _removeOrAddScrollFromBody(state) {
+		const stateOverFlowBody = state == "active" ? "hidden" : "scroll";
+		body.style.overflow = stateOverFlowBody;
+	}
+	static _eventFromBtnOpenModalListPlanets() {
+		const modalListPlanets = View._getElementFromId({ id: "list_planets" });
+		modalListPlanets.classList.toggle("active");
 
-    modalListPlanets.classList.toggle("active");
+		const actualStateFromModalList = modalListPlanets.classList[0];
+		View._removeOrAddScrollFromBody(actualStateFromModalList);
+	}
+	// Menu desktop methods
+	static _setMenuDesktop() {
+		return;
+		f;
+	}
 
-    // View.removeScrollFromBody();
-  }
-  // Menu desktop methods
-  static _setMenuDesktop() {
-    return;
-    f;
-  }
+	//methods
 
-  //methods
+	static setEventClickFromBtnPlanetOptionMobile(command) {
+		const btnsPlanetOptionMobile = View._getElementsFromClass({
+			className: "planet_option_mobile",
+		});
 
-  static setEventClickFromBtnPlanetOptionMobile(command) {
-    const btnsPlanetOptionMobile = View._getElementsFromClass({
-      className: "planet_option_mobile",
-    });
-
-    btnsPlanetOptionMobile.forEach((btn) => {
-      btn.addEventListener("click", (args) =>
-        View._eventClickFromBtnPlanetOptionMobile(args, command)
-      );
-    });
-  }
-  static _eventClickFromBtnPlanetOptionMobile(e, command) {
-    command(e.currentTarget.id);
-  }
+		btnsPlanetOptionMobile.forEach((btn) => {
+			btn.addEventListener("click", (args) =>
+				View._eventClickFromBtnPlanetOptionMobile(args, command)
+			);
+		});
+	}
+	static _eventClickFromBtnPlanetOptionMobile(e, command) {
+		command(e.currentTarget.id);
+	}
 }
