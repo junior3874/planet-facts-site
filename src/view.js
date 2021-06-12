b5786db1fc4f681675af0381111b62ab4255e3c7
junior3874 <wellingtonjr53@outlook.com.br>
2021-06-12 13:04:33 -0300

    finalized active current menu and set image with a menu

--- src/view.js
+++ src/view.js
@@ -13,166 +13,212 @@ const planetRevolution = document.getElementById("planet_prop_revolution_time");
 const planetRadius = document.getElementById("planet_prop_radius");
 const planetTemperature = document.getElementById("planet_prop_average_temp");
 
+const planetImage = document.getElementById("planet_image");
+const planetImageSrc = document.getElementById("planet_image_src");
 const btnsMenuOptionPlanet = document.querySelectorAll(
-  '[class="planet_menu_option"]'
+	'[class="planet_menu_option"]'
 );
 
 export default class View {
-  static currentMenu = {};
-  static currentPlanet = {};
-  static command;
+	static currentMenu = {};
+	static currentPlanet = {};
+	static command;
 
-  static loadingPlanets(planets) {
-    this.planetNames = planets.map(({ name }) => name);
-    const firstPlanet = planets[0];
-    View.setPlanetInPage(firstPlanet);
-  }
+	static loadingPlanets(planets) {
+		this.planetNames = planets.map(({ name }) => name);
+		const firstPlanet = planets[0];
+		View.setPlanetInPage(firstPlanet);
+	}
 
-  static setPlanetInPage(planet) {
-    this.currentPlanet = planet;
+	static setPlanetInPage(planet) {
+		this.currentPlanet = planet;
 
-    const initialMenuPlanet = this.currentPlanet.overview;
+		const initialMenuPlanet = this.currentPlanet.overview;
+		initialMenuPlanet.menuPropName = "overview";
 
-    View._setMenuPlanet(initialMenuPlanet);
-    View._setPlanetName();
-    View._setPlanetProp();
-  }
+		View._setMenuPlanet(initialMenuPlanet);
+		View._setPlanetName();
+		View._setPlanetProp();
+	}
 
-  static setCommandForGetPlanet(command) {
-    this.command = command;
-    return this;
-  }
+	static setCommandForGetPlanet(command) {
+		this.command = command;
+		return this;
+	}
 
-  static _setPlanetName() {
-    const planetNameInHtml = this.currentPlanet.name;
-    planetName.innerHTML = planetNameInHtml;
-  }
-  static _setPlanetProp() {
-    const { rotation, revolution, radius, temperature } = this.currentPlanet;
+	static _setPlanetName() {
+		const planetNameInHtml = this.currentPlanet.name;
+		planetName.innerHTML = planetNameInHtml;
+	}
+	static _setPlanetProp() {
+		const { rotation, revolution, radius, temperature } = this.currentPlanet;
 
-    planetRotation.innerHTML = rotation;
-    planetRevolution.innerHTML = revolution;
-    planetRadius.innerHTML = radius;
-    planetTemperature.innerHTML = temperature;
-  }
+		planetRotation.innerHTML = rotation;
+		planetRevolution.innerHTML = revolution;
+		planetRadius.innerHTML = radius;
+		planetTemperature.innerHTML = temperature;
+	}
 
-  static _setMenuPlanet(menu) {
-    this.currentMenuProxy.data = menu;
-  }
+	static _setMenuPlanet(menu) {
+		this.currentMenuProxy.data = menu;
+	}
 
-  static initializeMonitoringPlanetMenu() {
-    this.currentMenuProxy = new Proxy(this.currentMenu, {
-      set: function (target, key, value) {
-        if (value === View.currentMenu) return true;
-        target = value;
-        View._onSetMenu(value);
-        return true;
-      },
-    });
-  }
+	static initializeMonitoringPlanetMenu() {
+		this.currentMenuProxy = new Proxy(this.currentMenu, {
+			set: function (target, key, value) {
+				if (value === View.currentMenu) return true;
+				target = value;
+				View._onSetMenu(value);
+				return true;
+			},
+		});
+	}
+	static _removeActiveInOldMenu() {
+		const { menuPropName } = View.currentMenu;
 
-  static _onSetMenu(value) {
-    this.currentMenu = value;
-    View._onSetMenuSource();
-    View._onSetMenuImage();
-  }
+		const menuPropHtmlElement = View._getElementFromId({ id: menuPropName });
+		menuPropHtmlElement ? menuPropHtmlElement.classList.toggle("active") : null;
+	}
+	static _setActiveMenuInCurrentMenu() {
+		const { menuPropName } = View.currentMenu;
 
-  static _onSetMenuImage() {}
+		const menuPropHtmlElement = View._getElementFromId({ id: menuPropName });
+		menuPropHtmlElement ? menuPropHtmlElement.classList.toggle("active") : null;
+	}
+	static _onSetMenu(value) {
+		View._removeActiveInOldMenu();
+		this.currentMenu = value;
+		View._setActiveMenuInCurrentMenu();
+		View._onSetMenuSource();
+		View._onSetMenuImage();
+	}
 
-  static _onSetMenuSource() {
-    const textOnElementP = this.currentMenu.content;
-    planetSourceP.innerHTML = textOnElementP;
+	static _removeGeologyImage() {
+		const geologyImage = View._getElementFromId({ id: "geology-image" });
+		geologyImage ? planetImage.removeChild(geologyImage) : null;
+	}
 
-    const textOnElementWikipediaLink = this.currentMenu.source;
+	static _onSetMenuImage() {
+		const { name: planetName } = this.currentPlanet;
+		const { menuPropName: menuProp } = this.currentMenu;
 
-    planetSourceLink.href = textOnElementWikipediaLink;
-  }
+		let imageUrl;
 
-  static initializeMobileViewPort() {
-    View._monitoringMobileViewPort();
-    mobileViewPort.addListener(View._monitoringMobileViewPort);
-  }
+		View._removeGeologyImage();
 
-  static _monitoringMobileViewPort() {
-    View._removeOldContentHeader();
-    mobileViewPort.matches ? View._setMenuMobile() : View._setMenuDesktop();
-  }
+		switch (menuProp) {
+			case "structure":
+				imageUrl = `planet-${planetName}-internal.svg`;
+				break;
 
-  static _removeOldContentHeader() {
-    const htmlNavChild = View._getElementFromId({ id: "menu_select_mobile" });
-    htmlNavChild ? navContent.removeChild(htmlNavChild) : null;
-  }
+			case "geology":
+				imageUrl = `planet-${planetName}.svg`;
+				const chialdImage = document.createElement("img");
+				chialdImage.src = `./src/assets/geology-${planetName}.png`;
+				chialdImage.id = "geology-image";
+				planetImage.appendChild(chialdImage);
+				break;
+			case "overview":
+				imageUrl = `planet-${planetName}.svg`;
+		}
 
-  static _getElementFromId({ id, baseElement = document }) {
-    const existingItem = baseElement.querySelector(`[id="${id}"]`);
-    return existingItem;
-  }
+		const baseImageUrl = `./src/assets/${imageUrl}`;
+		planetImageSrc.src = baseImageUrl;
+	}
 
-  static _getElementsFromClass({ className, baseElement = document }) {
-    const existingItem = baseElement.querySelectorAll(`[class=${className}]`);
-    return existingItem;
-  }
+	static _onSetMenuSource() {
+		const textOnElementP = this.currentMenu.content;
+		planetSourceP.innerHTML = textOnElementP;
 
-  // Menu mobile methods
-  static async _setMenuMobile() {
-    const htmlTemplateMenuMobile = getMenuMobileTemplate(this.planetNames);
+		const textOnElementWikipediaLink = this.currentMenu.source;
+		planetSourceLink.href = textOnElementWikipediaLink;
+	}
 
-    navContent.innerHTML = `${htmlTemplateMenuMobile}`;
-    this.setEventClickFromBtnPlanetOptionMobile();
-    this.addEventFromBtnOpenModalListPlanets();
-  }
-  static addEventFromBtnOpenModalListPlanets() {
-    const btnListPlanets = View._getElementFromId({ id: "menu_select_planet" });
-    btnListPlanets.addEventListener(
-      "click",
-      View._eventFromBtnOpenModalListPlanets
-    );
-  }
+	static initializeMobileViewPort() {
+		View._monitoringMobileViewPort();
+		mobileViewPort.addListener(View._monitoringMobileViewPort);
+	}
 
-  static _removeOrAddScrollFromBody(state) {
-    const stateOverFlowBody = state == "active" ? "hidden" : "scroll";
-    body.style.overflow = stateOverFlowBody;
-  }
-  static _eventFromBtnOpenModalListPlanets() {
-    const modalListPlanets = View._getElementFromId({ id: "list_planets" });
-    modalListPlanets.classList.toggle("active");
+	static _monitoringMobileViewPort() {
+		View._removeOldContentHeader();
+		mobileViewPort.matches ? View._setMenuMobile() : View._setMenuDesktop();
+	}
 
-    const actualStateFromModalList = modalListPlanets.classList[0];
-    View._removeOrAddScrollFromBody(actualStateFromModalList);
-  }
-  // Menu desktop methods
-  static _setMenuDesktop() {
-    return;
-  }
+	static _removeOldContentHeader() {
+		const htmlNavChild = View._getElementFromId({ id: "menu_select_mobile" });
+		htmlNavChild ? navContent.removeChild(htmlNavChild) : null;
+	}
 
-  //methods
+	static _getElementFromId({ id, baseElement = document }) {
+		const existingItem = baseElement.querySelector(`[id="${id}"]`);
+		return existingItem;
+	}
 
-  static setEventClickForBtnMenuPlanetOption() {
-    btnsMenuOptionPlanet.forEach((btn) => {
-      btn.addEventListener("click", View._setNewMenuFromPlanetPage);
-    });
-  }
+	static _getElementsFromClass({ className, baseElement = document }) {
+		const existingItem = baseElement.querySelectorAll(`[class=${className}]`);
+		return existingItem;
+	}
 
-  static _setNewMenuFromPlanetPage(event) {
-    const menuProp = event.currentTarget.id;
-    const newMenuProp = View.currentPlanet[menuProp];
+	// Menu mobile methods
+	static async _setMenuMobile() {
+		const htmlTemplateMenuMobile = getMenuMobileTemplate(this.planetNames);
 
-    View.currentMenuProxy.data = newMenuProp;
-  }
+		navContent.innerHTML = `${htmlTemplateMenuMobile}`;
+		this.setEventClickFromBtnPlanetOptionMobile();
+		this.addEventFromBtnOpenModalListPlanets();
+	}
+	static addEventFromBtnOpenModalListPlanets() {
+		const btnListPlanets = View._getElementFromId({ id: "menu_select_planet" });
+		btnListPlanets.addEventListener(
+			"click",
+			View._eventFromBtnOpenModalListPlanets
+		);
+	}
 
-  static setEventClickFromBtnPlanetOptionMobile() {
-    const btnsPlanetOptionMobile = View._getElementsFromClass({
-      className: "planet_option_mobile",
-    });
+	static _removeOrAddScrollFromBody(state) {
+		const stateOverFlowBody = state == "active" ? "hidden" : "scroll";
+		body.style.overflow = stateOverFlowBody;
+	}
+	static _eventFromBtnOpenModalListPlanets() {
+		const modalListPlanets = View._getElementFromId({ id: "list_planets" });
+		modalListPlanets.classList.toggle("active");
 
-    btnsPlanetOptionMobile.forEach((btn) => {
-      btn.addEventListener("click", (args) => {
-        View._getPlanetPage(args, View.command);
-        return View._eventFromBtnOpenModalListPlanets();
-      });
-    });
-  }
-  static _getPlanetPage(e, command) {
-    command(e.currentTarget.id);
-  }
+		const actualStateFromModalList = modalListPlanets.classList[0];
+		View._removeOrAddScrollFromBody(actualStateFromModalList);
+	}
+	// Menu desktop methods
+	static _setMenuDesktop() {
+		return;
+	}
+
+	//methods
+
+	static setEventClickForBtnMenuPlanetOption() {
+		btnsMenuOptionPlanet.forEach((btn) => {
+			btn.addEventListener("click", View._setNewMenuFromPlanetPage);
+		});
+	}
+
+	static _setNewMenuFromPlanetPage(event) {
+		const menuProp = event.currentTarget.id;
+		const newMenuProp = View.currentPlanet[menuProp];
+		newMenuProp.menuPropName = menuProp;
+		View.currentMenuProxy.data = newMenuProp;
+	}
+
+	static setEventClickFromBtnPlanetOptionMobile() {
+		const btnsPlanetOptionMobile = View._getElementsFromClass({
+			className: "planet_option_mobile",
+		});
+
+		btnsPlanetOptionMobile.forEach((btn) => {
+			btn.addEventListener("click", (args) => {
+				View._getPlanetPage(args, View.command);
+				return View._eventFromBtnOpenModalListPlanets();
+			});
+		});
+	}
+	static _getPlanetPage(e, command) {
+		command(e.currentTarget.id);
+	}
 }
