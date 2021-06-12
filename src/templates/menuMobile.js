8b23e6caa545c8aed5558f05c4d04385990138f5
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-06-11 21:34:20 -0300

    added other div for element

--- src/templates/menuMobile.js
+++ src/templates/menuMobile.js
@@ -3,11 +3,13 @@ import getPlanetOptionMobileTemplate from "./planetOptionMobile.js";
 const menuMobileIcon = "./src/assets/icon-hamburger.svg";
 export default function getMenuMobileTemplate(planetNames) {
   return `
+  <div id="menu_select_mobile">
     <a id="menu_select_planet">
         <img class="icon-hamburger"src="${menuMobileIcon}" alt="">
     </a>
     <ul id="list_planets">
       ${getPlanetOptionMobileTemplate(planetNames)}
     </ul>
+    </div>
   `;
 }
